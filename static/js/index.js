c3bcbce608d5eda3f4b5003b76ee11631bff4fef
Trent Waskey <trent.waskey@metricly.com>
2019-03-06 15:59:08 -0500

    Added fixed header

--- static/js/index.js
+++ static/js/index.js
@@ -2403,3 +2403,13 @@
     }))
   }
 }]);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").classList.add("scrolled");
+  } else {
+    document.getElementById("header").classList.remove("scrolled");
+  }
+}
